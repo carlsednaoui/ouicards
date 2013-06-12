f301893998c040b9ab9a19b340cd7c9908792b6f
carl sednaoui <carlsednaoui@gmail.com>
2013-06-12 13:21:45 -0400

    Start small refactor

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 
   // If answer is hidden, show it. Otherwise create a new question and insert it.
   $('.generate').on('click', function() {
-    $('.answer').is(':visible') ? createAndChangeQuestion(flashcards) : $('.answer').show();
+    $('.answer').is(':visible') ? changeQuestion(flashcards) : $('.answer').show();
   });
 
   // Same thing with the loop-thru-saved-questions button.
   $('.loop-thru-saved-questions').on('click', function() {
-    $('.answer').is(':visible') ? createAndChangeQuestion(flashcards, getQuestionsFromLocalStorage()) : $('.answer').show();
+    $('.answer').is(':visible') ? changeQuestion(flashcards, getFromLocalStorage()) : $('.answer').show();
   });
 
   // Display answer if user clicks on question.
@@ -25,24 +25,23 @@ $(document).ready(function() {
     var questionID = parseInt($('.question p').attr('id'));
 
     if (questionID) {
-      saveQuestionToLocalStorage(questionID);
-      $('.save').html("Saved: " + getQuestionsFromLocalStorage().length + " question");
+      saveToLocalStorage(questionID);
+      $('.save').html("Saved: " + getFromLocalStorage().length + " question");
     }
   });
 
   // Update button copy if we have saved questionss.
-  if (getQuestionsFromLocalStorage()) {
-    $('.save').html("Saved: " + getQuestionsFromLocalStorage().length + " question");
+  if (getFromLocalStorage()) {
+    $('.save').html("Saved: " + getFromLocalStorage().length + " question");
   }
 });
 
 // Generate a new question/ answer object.
-function createRandomQuestion(questions, array) {
+function createQuestion(questions, array) {
   var randomNumber, questionParagraph, answerParagraph;
 
   // If you pass an array, pick a random number from it. Otherwise create a random number based on the numer of questions passed.
   randomNumber = array ? array[Math.floor(Math.random() * array.length)] : Math.floor(Math.random() * questions.length);
-  console.log(randomNumber);
 
   questionParagraph = document.createElement('p');
   questionParagraph.id = randomNumber;
@@ -55,12 +54,12 @@ function createRandomQuestion(questions, array) {
 }
 
 // Change the actual page HTML with a new question/ answer.
-function createAndChangeQuestion(questions, array) {
-  var questionAnswerObject = createRandomQuestion(questions, array);
+function changeQuestion(questions, array) {
+  var questionObject = createQuestion(questions, array);
 
   $('.answer').hide();
-  $('.question').html(questionAnswerObject.question);
-  $('.answer').html(questionAnswerObject.answer);
+  $('.question').html(questionObject.question);
+  $('.answer').html(questionObject.answer);
 }
 
 function getQuestion(questionNumber) {
@@ -71,33 +70,27 @@ function getAnswer(questionNumber) {
   return flashcards[questionNumber].answer;
 }
 
-function createRandomQuestionFromArrayIDS(arrayIDS) {
-  var randomNumber = Math.floor(Math.random() * arrayIDS.length);
-  $('.question').html(getQuestion(arrayIDS[randomNumber]));
-  $('.answer').html("<b>Answer:</b> <br/>" + getAnswer(arrayIDS[randomNumber]).replace(/\n/g,"<br>"));
-}
-
-function saveQuestionToLocalStorage(questionID) {
-  var savedCards = getQuestionsFromLocalStorage();
+function saveToLocalStorage(questionID) {
+  var savedCards = getFromLocalStorage();
 
   // Not sure if there's a better way to do this without if/ else.
   if ( savedCards && (savedCards.indexOf(questionID) === -1) ) {
-    localStorage.saved_naturalization_flashcard_questions = savedCards + ',' + questionID;
+    localStorage.saved_questions = savedCards + ',' + questionID;
   } else if ( !savedCards ) {
-    localStorage.saved_naturalization_flashcard_questions = questionID;
+    localStorage.saved_questions = questionID;
   }
 }
 
-function getQuestionsFromLocalStorage() {
+function getFromLocalStorage() {
   // Used to avoid JS errors. Not sure if this is the best way to do this.
-  if (!localStorage.saved_naturalization_flashcard_questions) {
+  if (!localStorage.saved_questions) {
     return;
   }
-  var savedQuestionArray = localStorage.saved_naturalization_flashcard_questions.split(',');
-  var savedQuestionArrayInIntegers = savedQuestionArray.map(function(el) { return parseInt(el, 10); });
-  return savedQuestionArrayInIntegers;
+  var savedQuestions = localStorage.saved_questions.split(',');
+  var savedQuestionsInt = savedQuestions.map(function(el) { return parseInt(el, 10); });
+  return savedQuestionsInt;
 }
 
 function clearlLocalStorage() {
-  delete localStorage.saved_naturalization_flashcard_questions;
+  delete localStorage.saved_questions;
 }
