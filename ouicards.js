ecc8a3f22b0a8808af156ef36118735feea99467
carl sednaoui <carlsednaoui@gmail.com>
2013-06-26 19:08:59 -0400

    Refactor ouicards.js to abstract logic and allow it to ONLY handle flashcard specific logic

--- ouicards.js
+++ ouicards.js
@@ -1,33 +1,33 @@
 (function(exports) {
   exports.ouicards = {
-    changeQuestion: function(questionsArray, flashcards, questionElSelector, answerElSelector) {
-      // Prevent user from looping thru an empty questionsArray or empty flashcards
-      if (!questionsArray || questionsArray.length === 0 || !flashcards || flashcards.length === 0 ) {
+    changeQuestion: function(arrayForRdnNm, flashcards, qSelector, aSelector) {
+      // Prevent user from looping thru an empty arrayForRdnNm or empty flashcards
+      if (!arrayForRdnNm || arrayForRdnNm.length === 0 || !flashcards || flashcards.length === 0 ) {
         console.log('what are you doing?');
         return;
       }
 
-      var randomNumber = this.generateRandomNumber(questionsArray, flashcards);
+      var randomNumber = this.generateRandomNumber(arrayForRdnNm, flashcards);
       var currentQuestionId = parseInt($('.question p').attr('id'), 10);
 
       // Ensure same question is not displayed twice in a row
-      // Do this only if we have more than 1 question in our questionsArray
-      while (randomNumber === currentQuestionId && questionsArray.length > 1) {
-        randomNumber = this.generateRandomNumber(questionsArray, flashcards);
+      // Do this only if we have more than 1 question in our arrayForRdnNm
+      while (randomNumber === currentQuestionId && arrayForRdnNm.length > 1) {
+        randomNumber = this.generateRandomNumber(arrayForRdnNm, flashcards);
       }
 
       var questionObject = this.buildQuestionHTML(randomNumber, flashcards);
 
-      $(answerElSelector).hide();
-      $(questionElSelector).html(questionObject.question);
-      $(answerElSelector).html(questionObject.answer);
+      $(aSelector).hide();
+      $(qSelector).html(questionObject.question);
+      $(aSelector).html(questionObject.answer);
     },
-    generateRandomNumber: function(questionsArray, flashcards) {
-      // If the questionsArray passed is === flashcards, just create a random number
-      // otherwise extract a random number from the questionsArray.
-      var randomNumber = questionsArray === flashcards ?
-          Math.floor(Math.random() * questionsArray.length) :
-          questionsArray[Math.floor(Math.random() * questionsArray.length)];
+    generateRandomNumber: function(arrayForRdnNm, flashcards) {
+      // If the arrayForRdnNm passed is === flashcards, just create a random number
+      // otherwise extract a random number from the arrayForRdnNm.
+      var randomNumber = arrayForRdnNm === flashcards ?
+          Math.floor(Math.random() * arrayForRdnNm.length) :
+          arrayForRdnNm[Math.floor(Math.random() * arrayForRdnNm.length)];
       return randomNumber;
     },
     buildQuestionHTML: function(questionNumber, flashcards) {
