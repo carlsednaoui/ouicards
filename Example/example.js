0de9f71358300b35e6f365572dde7d93f03b7391
carl sednaoui <carlsednaoui@gmail.com>
2013-06-26 16:04:03 -0400

    Rename Example/main.js to Example/example.js, also fix Example/example.js variable names

---
+++ Example/example.js
@@ -0,0 +1,30 @@
+document.getElementById('pre-load-button').onclick = function() {
+  showQuestionInput();
+};
+
+document.getElementById('load-questions').onclick = function() {
+  initializeHandlers(getUserQuestions());
+};
+
+function showQuestionInput() {
+  document.getElementById('questions-input-area').style.display = 'block';
+  document.getElementById('load-questions').style.display = 'block';
+  document.getElementById('pre-load-button').style.display = 'none';
+}
+
+function getUserQuestions() {
+  var flashcards = [],
+      userInput = document.getElementById('questions-input-area').value.split('\n');
+
+  // Get rid of empty questions
+  userInput = userInput.filter(function(card) {
+    return card !== "";
+  });
+
+  userInput.forEach(function(card) {
+    var parsedCard = card.split('\t');
+    flashcards.push({question: parsedCard[0], answer: parsedCard[1]});
+  });
+
+  return flashcards;
+}
