a67ef0bc7d2e4f91c62e09bb8dcbe43eab1932f2
carl sednaoui <carlsednaoui@gmail.com>
2013-07-08 11:47:09 -0400

    Refactor ouicards.js to better use the exports variable

--- ouicards.js
+++ ouicards.js
@@ -1,148 +1,171 @@
 (function(exports) {
+  function loadFromArray(array) {
+    ouicards.flashcards = array;
+    resetBuckets();
+  }
+
+  function loadFromBrowser(selector, delimiter) {
+    var _flashcards = [],
+        userInput  = $(selector).val().split('\n');
+
+    // Get rid of empty questions
+    userInput = userInput.filter(function(card) {
+       return card !== "";
+     });
+
+    if (userInput.length === 0) {
+      console.log('There are no flashcards to upload.');
+      return;
+    }
+
+    userInput.forEach(function(card) {
+      var parsedCard = card.split(delimiter);
+      _flashcards.push({question: parsedCard[0], answer: parsedCard[1]});
+    });
+
+    ouicards.flashcards = _flashcards;
+    resetBuckets();
+    return getFromLS();
+  }
+
+  function next() {
+    var newQuestion,
+        bigInterval   = Math.ceil(ouicards.flashcards.length / 3) + 1,
+        smallInterval = Math.ceil(ouicards.flashcards.length / 6) + 1;
+
+    // Show an answer from bucket C once every bigInterval 
+    // So long as Bucket C it's not empty
+    if (ouicards.counter % bigInterval === 0 && ouicards.bucketC.length !== 0) {
+      newQuestion = getQuestion(ouicards.bucketC);
+      ouicards.currentBucket = ouicards.bucketC;
+
+    // Show an answer from bucket B once every smallInterval
+    // So long as Bucket B it's not empty
+    } else if (ouicards.counter % smallInterval === 0 && ouicards.bucketB.length !== 0) {
+      newQuestion = getQuestion(ouicards.bucketB);
+      ouicards.currentBucket = ouicards.bucketB;
+
+    // Show an answer from Bucket A, so long as it's not empty
+    } else if (ouicards.bucketA.length !== 0) {
+      newQuestion = getQuestion(ouicards.bucketA);
+      ouicards.currentBucket = ouicards.bucketA;
+
+    // Show an answer from Bucket B, so long as it's not empty
+    } else if (ouicards.bucketB.length !== 0) {
+      newQuestion = getQuestion(ouicards.bucketB);
+      ouicards.currentBucket = ouicards.bucketB;
+
+    // Show a question from Bucket C, so long as it's not empty
+    } else if (ouicards.bucketC.length !== 0) {
+      newQuestion = getQuestion(ouicards.bucketC);
+      ouicards.currentBucket = ouicards.bucketC;
+    } else {
+      console.log('There was a serious problem with ouicards. You should never see ');
+    }
+
+    // Reset ouicards.counter if it's greater than flashcard count, otherwise ++ it
+    ouicards.counter >= ouicards.flashcards.length ? ouicards.counter = 1 : ouicards.counter++;
+    return newQuestion;
+  }
+
+  function correct() {
+    // Do nothing if ouicards.counter is at initial state
+    if (ouicards.counter === 1)
+      return;
+
+    if (ouicards.currentBucket === ouicards.bucketA) {
+      moveQuestion(ouicards.bucketA, ouicards.bucketB);
+    } else if (ouicards.currentBucket === ouicards.bucketB) {
+      moveQuestion(ouicards.bucketB, ouicards.bucketC);
+    } else if (ouicards.currentBucket === ouicards.bucketC) {
+      moveQuestion(ouicards.bucketC, ouicards.bucketC);
+    } else
+      console.log('Hmm, you should not be here.');
+    saveToLS();
+  }
+
+  function wrong() {
+    console.log(ouicards.counter);
+    // Do nothing if ouicards.counter is at initial state
+    if (ouicards.counter === 1)
+      return;
+
+    moveQuestion(ouicards.currentBucket, ouicards.bucketA);
+    saveToLS();
+  }
+
+  function moveQuestion(fromBucket, toBucket) {
+    toBucket.push(fromBucket.shift());
+  }
+
+  function getQuestion(bucket) {
+    // Prevent from looping thru an empty bucket
+    if (!bucket || bucket.length === 0) {
+      console.log("You can't load an empty set of questions.");
+      return;
+    }
+
+    return buildQuestionHTML(bucket[0]);
+  }
+
+  function buildQuestionHTML(rawQuestion) {
+    var questionEl, answerEl;
+
+    questionEl = document.createElement('p');
+    questionEl.innerHTML = rawQuestion.question;
+
+    answerEl = document.createElement('p');
+    answerEl.innerHTML = rawQuestion.answer.replace(/\n/g, '<br>');
+
+    return {question: questionEl, answer: answerEl};
+  }
+
+  function saveToLS() {
+    localStorage.flashcards = JSON.stringify(ouicards.flashcards);
+    localStorage.bucketA    = JSON.stringify(ouicards.bucketA);
+    localStorage.bucketB    = JSON.stringify(ouicards.bucketB);
+    localStorage.bucketC    = JSON.stringify(ouicards.bucketC);
+  }
+
+  function getFromLS() {
+    ouicards.flashcards    = JSON.parse(localStorage.flashcards || '[]');
+    ouicards.bucketA       = JSON.parse(localStorage.bucketA    || '[]');
+    ouicards.bucketB       = JSON.parse(localStorage.bucketB    || '[]');
+    ouicards.bucketC       = JSON.parse(localStorage.bucketC    || '[]');
+    ouicards.currentBucket = ouicards.bucketA.length ? ouicards.bucketA :
+                         ouicards.bucketB.length ? ouicards.bucketB :
+                         ouicards.bucketC.length ? ouicards.bucketC : [];
+
+    ouicards.counter = 1;
+    return {flashcards: ouicards.flashcards, bucketA: ouicards.bucketA, bucketB: ouicards.bucketB, bucketC: ouicards.bucketC};
+  }
+
+  function resetBuckets() {
+    ouicards.bucketA       = ouicards.flashcards.slice(0);
+    ouicards.currentBucket = ouicards.bucketA;
+    ouicards.bucketB       = [];
+    ouicards.bucketC       = [];
+    ouicards.counter       = 1;
+    saveToLS();
+  }
+
   exports.ouicards = {
     currentBucket: [],
-    flashcards: [],
-    bucketA: [],
-    bucketB: [],
-    bucketC: [],
-    counter: 1,
-    loadFromArray: function(array) {
-      this.flashcards = array;
-      this.resetBuckets();
-    },
-    loadFromBrowser: function(selector, delimiter) {
-      var flashcards = [],
-          userInput  = $(selector).val().split('\n');
-
-      // Get rid of empty questions
-      userInput = userInput.filter(function(card) {
-         return card !== "";
-       });
-
-      if (userInput.length === 0) {
-        console.log('There are no flashcards to upload.');
-        return;
-      }
-
-      userInput.forEach(function(card) {
-        var parsedCard = card.split(delimiter);
-        flashcards.push({question: parsedCard[0], answer: parsedCard[1]});
-      });
-
-      this.flashcards = flashcards;
-      this.resetBuckets();
-      return this.getFromLS();
-    },
-    next: function() {
-      var newQuestion,
-          bigInterval   = Math.ceil(this.flashcards.length / 3) + 1,
-          smallInterval = Math.ceil(this.flashcards.length / 6) + 1;
-
-      // Show an answer from bucket C once every bigInterval 
-      // So long as Bucket C it's not empty
-      if (this.counter % bigInterval === 0 && this.bucketC.length !== 0) {
-        newQuestion = this.getQuestion(this.bucketC);
-        this.currentBucket = this.bucketC;
-
-      // Show an answer from bucket B once every smallInterval
-      // So long as Bucket B it's not empty
-      } else if (this.counter % smallInterval === 0 && this.bucketB.length !== 0) {
-        newQuestion = this.getQuestion(this.bucketB);
-        this.currentBucket = this.bucketB;
-
-      // Show an answer from Bucket A, so long as it's not empty
-      } else if (this.bucketA.length !== 0) {
-        newQuestion = this.getQuestion(this.bucketA);
-        this.currentBucket = this.bucketA;
-
-      // Show an answer from Bucket B, so long as it's not empty
-      } else if (this.bucketB.length !== 0) {
-        newQuestion = this.getQuestion(this.bucketB);
-        this.currentBucket = this.bucketB;
-
-      // Show a question from Bucket C, so long as it's not empty
-      } else if (this.bucketC.length !== 0) {
-        newQuestion = this.getQuestion(this.bucketC);
-        this.currentBucket = this.bucketC;
-      } else {
-        console.log('There was a serious problem with ouicards. You should never see this.');
-      }
-
-      // Reset counter if it's greater than flashcard count, otherwise ++ it
-      this.counter >= this.flashcards.length ? this.counter = 1 : this.counter++;
-      return newQuestion;
-    },
-    correct: function() {
-      // Do nothing if counter is at initial state
-      if (this.counter === 1)
-        return;
-
-      if (this.currentBucket === this.bucketA) {
-        this.moveQuestion(this.bucketA, this.bucketB);
-      } else if (this.currentBucket === this.bucketB) {
-        this.moveQuestion(this.bucketB, this.bucketC);
-      } else if (this.currentBucket === this.bucketC) {
-        this.moveQuestion(this.bucketC, this.bucketC);
-      } else
-        console.log('Hmm, you should not be here.');
-      this.saveToLS();
-    },
-    wrong: function() {
-      // Do nothing if counter is at initial state
-      if (this.counter === 1)
-        return;
-
-      this.moveQuestion(this.currentBucket, this.bucketA);
-      this.saveToLS();
-    },
-    moveQuestion: function(fromBucket, toBucket) {
-      toBucket.push(fromBucket.shift());
-    },
-    getQuestion: function(bucket) {
-      // Prevent from looping thru an empty bucket
-      if (!bucket || bucket.length === 0) {
-        console.log("You can't load an empty set of questions.");
-        return;
-      }
-
-      return this.buildQuestionHTML(bucket[0]);
-    },
-    buildQuestionHTML: function(rawQuestion) {
-      var questionEl, answerEl;
-
-      questionEl = document.createElement('p');
-      questionEl.innerHTML = rawQuestion.question;
-
-      answerEl = document.createElement('p');
-      answerEl.innerHTML = rawQuestion.answer.replace(/\n/g, '<br>');
-
-      return {question: questionEl, answer: answerEl};
-    },
-    saveToLS: function() {
-      localStorage.flashcards = JSON.stringify(this.flashcards);
-      localStorage.bucketA    = JSON.stringify(this.bucketA);
-      localStorage.bucketB    = JSON.stringify(this.bucketB);
-      localStorage.bucketC    = JSON.stringify(this.bucketC);
-    },
-    getFromLS: function() {
-      this.flashcards    = JSON.parse(localStorage.flashcards || '[]');
-      this.bucketA       = JSON.parse(localStorage.bucketA    || '[]');
-      this.bucketB       = JSON.parse(localStorage.bucketB    || '[]');
-      this.bucketC       = JSON.parse(localStorage.bucketC    || '[]');
-      this.currentBucket = this.bucketA.length ? this.bucketA :
-                           this.bucketB.length ? this.bucketB :
-                           this.bucketC.length ? this.bucketC : [];
-
-      this.counter = 1;
-      return {flashcards: this.flashcards, bucketA: this.bucketA, bucketB: this.bucketB, bucketC: this.bucketC};
-    },
-    resetBuckets: function() {
-      this.bucketA       = this.flashcards.slice(0);
-      this.currentBucket = this.bucketA;
-      this.bucketB       = [];
-      this.bucketC       = [];
-      this.counter       = 1;
-      this.saveToLS();
-    }
+    flashcards:    [],
+    bucketA:       [],
+    bucketB:       [],
+    bucketC:       [],
+    counter:       1,
+    loadFromArray: loadFromArray,
+    loadFromBrowser: loadFromBrowser,
+    next: next,
+    correct: correct,
+    wrong: wrong,
+    moveQuestion: moveQuestion,
+    getQuestion: getQuestion,
+    buildQuestionHTML: buildQuestionHTML,
+    saveToLS: saveToLS,
+    getFromLS: getFromLS,
+    resetBuckets: resetBuckets
   };
 })(this);
